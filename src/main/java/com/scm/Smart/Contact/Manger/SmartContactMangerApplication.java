c342b9a6611c6133c0050a9cecdc2d572a1657c3
hardiksolanki24 <hardiksolanki200224@gmail.com>
2024-04-26 18:10:07 +0530

    Initial commit

---
+++ src/main/java/com/scm/Smart/Contact/Manger/SmartContactMangerApplication.java
@@ -0,0 +1,13 @@
+package com.scm.Smart.Contact.Manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactMangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scm/Smart/Contact/Manger/SmartContactMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.Smart.Contact.Manger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartContactMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
